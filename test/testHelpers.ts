4f534ae01784b3afe3ffde9c967e116d7a7bdaa1
Seth Orell <seth.orell@gmail.com>
2023-10-03 10:22:53 -0500

    Replace faker; update libs

--- test/testHelpers.ts
+++ test/testHelpers.ts
@@ -1,19 +1,19 @@
 import { ulid } from 'ulid';
-import faker from 'faker';
+import { faker } from '@faker-js/faker';
 
 import { User } from '../src/models';
 import UserRepository from '../src/uniqueConstraints';
 
 export const createRandomUser = (overrideWith?: Partial<User>): User => {
-  const firstName = faker.name.firstName();
-  const lastName = `${faker.name.lastName()}_${ulid()}`;
+  const firstName = faker.person.firstName();
+  const lastName = `${faker.person.lastName()}_${ulid()}`;
 
   return {
     id: ulid(),
     userName: `${firstName.toLowerCase()[0]}${lastName.toLowerCase()}`,
     email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@${faker.internet.domainName()}`,
     fullName: `${firstName} ${lastName}`,
-    phoneNumber: faker.phone.phoneNumber(),
+    phoneNumber: faker.phone.number(),
     ...overrideWith,
   };
 };
